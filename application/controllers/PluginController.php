b65f85e838bbd6d56f38d476104c532d0edac11e
Mark <mark560@gmail.com>
2013-05-17 12:37:57 +0800

    Change-Id: Id7a55e6f4ae3941e0002ccdf3beac97326b1cb01

---
+++ application/controllers/PluginController.php
@@ -0,0 +1,35 @@
+<?php
+
+class PluginController extends Zend_Controller_Action
+{
+  
+	
+    function init()
+    {
+        $this->db = Zend_Registry::get("db");
+    }
+	
+    function indexAction()
+    {
+     	die;
+    }
+    
+    function wsdlTestAction()
+    {
+    	$wsdl = new Core_Wsdl();
+    	
+    	$post_array = array(
+    			"request_type" => "DetectIdentity",
+    			"params" => array(
+    					"username" => "aa",
+    					"password" => md5("bb")
+    			)
+    	);
+    	
+    	$result = $wsdl->WsdlClient(1, $post_array);
+    	
+    	echo ">>".$result."<<";
+    	die;
+    }
+}
+
