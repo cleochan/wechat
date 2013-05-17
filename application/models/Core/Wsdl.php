85cea1394a0e144b0fa4c66002f26081923daaef
Mark <mark560@gmail.com>
2013-05-17 16:04:52 +0800

    Change-Id: I04c13fe30db5ebabf308d26f62ec3bc154ee5002

--- application/models/Core/Wsdl.php
+++ application/models/Core/Wsdl.php
@@ -14,8 +14,6 @@ class Core_Wsdl
 		$service_model = new Database_Table_SystemService();
 		$service_model->service_id = $service_id;
 		$client = $service_model->MakeServiceObject();
-		echo "[BEFORE]";
-		die;
 		$result = $client->S1($post_array); //post to server
 		print_r($result);
 		echo "[END]";
