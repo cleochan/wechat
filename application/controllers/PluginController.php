390e099675207d32027f09bceea81f4493fcc583
Mark <mark560@gmail.com>
2013-05-17 16:31:22 +0800

    Change-Id: Ie5734f838850a48b00735a3e77e36fde004de189

--- application/controllers/PluginController.php
+++ application/controllers/PluginController.php
@@ -21,14 +21,25 @@ class PluginController extends Zend_Controller_Action
     	$post_array = array(
     			"request_type" => "DetectIdentity",
     			"params" => array(
-    					"username" => "markchan",
-    					"password" => md5("07131921")
+    					"username" => "AAA",
+    					"password" => md5("BBB")
     			)
     	);
     	
     	$result = $wsdl->WsdlClient(1, $post_array);
     	
     	echo ">>".$result."<<";
+    	die;
+    }
+    
+    function testUserAction()
+    {
+    	$system_user_model = new Database_Table_SystemUser();
+    	$system_user_model->service_id = 1;
+    	$system_user_model->service_user_id = 22;
+    	$system_user_model->wechat_ref = 33;
+    	$system_user_model->UserRegistration();
+    	echo "End.";
     	die;
     }
 }
